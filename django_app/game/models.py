3bb6a56a01de57acef7a07a54c69c3019d922cdb
cstucke <smallchildcharlie@gmail.com>
2026-02-12 04:42:05 -1100

    feat: implemented Play model and updated git ignore

--- django_app/game/models.py
+++ django_app/game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Play(models.Model):
+    story_id = models.IntegerField()
+    
+    ending_page_id = models.IntegerField()
+    
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Story ID {self.story_id} played at {self.created_at}"
